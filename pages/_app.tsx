78499d48a52934e5e71d66bf29233580822ad0ce
Brendan McGuire <brendan@bren.app>
2021-09-26 23:34:49 -0400

    Default styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import "../main.css";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default App
+export default App;
