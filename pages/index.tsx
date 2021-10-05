8fc91d741fd3b8250425dec870e9847f6d48b7ed
Brendan McGuire <brendan@bren.app>
2021-10-05 13:56:35 -0400

    Handle server side rendering of settingspane

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,15 @@ import Head from "next/head";
 import { Input, Select } from "@components/Input";
 
 function SettingsPane() {
+  
   const [open, setOpen] = useState(false);
+  const [id, setID] = useState(() => {
+    if (process.browser) {
+      return localStorage.getItem("id") || "";
+    }
+
+    return "";
+  });
 
   return (
     <>
@@ -22,29 +30,18 @@ function SettingsPane() {
           stroke="currentColor"
           onClick={() => setOpen(!open)}
         >
-          {open ? (
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          ) : (
-            <>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-              />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </>
-          )}
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
         </svg>
       </div>
       <motion.div
@@ -64,7 +61,14 @@ function SettingsPane() {
             >
               Participant ID
             </label>
-            <Input id="participant-id" value={"1234"} className="border-0" />
+            <Input
+              id="participant-id"
+              type="number"
+              value={id}
+              onChange={(event) => setID(event.target.value)}
+              onBlur={() => localStorage.setItem("id", id)}
+              className="border-0"
+            />
           </div>
         </div>
       </motion.div>
@@ -129,7 +133,8 @@ export default function Home() {
         />
       </Head>
       <SettingsPane />
-      <PopupID />
+      { process.browser && <PopupID /> }
+
       <nav className="w-full">
         <div className="accent w-full h-2 bg-orange"></div>
       </nav>
