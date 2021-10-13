3b630d11937778d772fb024891fc02bf7b4c7556
Brendan McGuire <brendan@bren.app>
2021-10-13 13:52:39 -0400

    Deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+docker build -t bmmcgui/cu-smart .
+docker push bmmcgui/cu-smart
+
+ssh fmo14 "docker pull bmmcgui/cu-smart"
+ssh fmo14 "docker kill $(docker ps --filter="publish=3000" --format "{{.Names}}")"
+ssh fmo14 "docker run -p 3000:3000 -d -v cu-smart-feedback:/data --restart always bmmcgui/cu-smart"
