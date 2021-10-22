4a0758244fb9d9104ca58b84a7993797b735672b
Brendan McGuire <brendan@bren.app>
2021-10-22 15:51:06 -0400

    Explicit mounting for docker container

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ docker push bmmcgui/cu-smart
 
 ssh fmo14 "docker pull bmmcgui/cu-smart"
 ssh fmo14 'docker kill $(docker ps --filter="publish=3000" --format "{{.Names}}")'
-ssh fmo14 "docker run -p 3000:3000 -d -v cu-smart-feedback:/data --restart always bmmcgui/cu-smart"
+ssh fmo14 "docker run -p 3000:3000 -d --mount source=cu-smart-feedback,target=/data --restart always bmmcgui/cu-smart"
