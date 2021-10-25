d0d41317644ea036c4cd84ffe73b4b70f2e0c966
Brendan McGuire <brendan@bren.app>
2021-10-25 12:28:04 -0400

    Add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,31 @@
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="application-name" content={"CU Smart"} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={"CU Smart"} />
+        <meta
+          name="description"
+          content={
+            "Recommends the most comfortable space on Clemson's Campus for you based on your feedback."
+          }
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#F56600" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default App;
