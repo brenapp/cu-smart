c652f3bc78003473ca72695c33cf191ff03c5f7a
Brendan McGuire <brendan@bren.app>
2021-10-26 15:10:23 -0400

    Add progress bar to survey

--- components/Survey.tsx
+++ components/Survey.tsx
@@ -1,11 +1,20 @@
-import { motion } from "framer-motion";
+/**
+ * @author Brendan McGuire
+ * @date 28 September 2021
+ *
+ * Paginated Survey component, with a list of questions and a pagination bar, and animations between
+ * pages.
+ **/
+
+import { popIn } from "@lib/client/animations";
+import { motion, AnimatePresence } from "framer-motion";
 import { ReactChild, useState } from "react";
 import { Button, ButtonProps } from "./Input";
 
 type Props = {
   children: ReactChild[];
   onSubmit?: () => void;
-  submitButton?: Partial<ButtonProps>
+  submitButton?: Partial<ButtonProps>;
 };
 
 export default function Survey({ children, onSubmit, submitButton }: Props) {
@@ -18,10 +27,19 @@ export default function Survey({ children, onSubmit, submitButton }: Props) {
   return (
     <div>
       <h3 className="text-base font-semibold uppercase">Survey</h3>
-      <h5 className="text-gray-500">
-        Question {index + 1} of {children.length}
-      </h5>
-      <div>{children[index]}</div>
+      <div className="flex items-center mb-2">
+        <h5 className="text-gray-500">
+          Question {index + 1} of {children.length}
+        </h5>
+        <div className="progress relative ml-4 flex-1 h-2">
+          <div
+            className="progress-bar bg-orange h-2 rounded-md absolute z-10 transition-all"
+            style={{ width: `${((index + 1) / children.length) * 100}%` }}
+          />
+          <div className="remainder absolute h-2 w-full bg-gray-200 rounded-md"></div>
+        </div>
+      </div>
+      {children[index]}
       <div className="flex w-full items-center justify-end">
         <Button
           text="Previous"
@@ -29,10 +47,16 @@ export default function Survey({ children, onSubmit, submitButton }: Props) {
           className="mr-2"
           onClick={decrement}
         />
+
         {index < children.length - 1 ? (
           <Button text="Next" variant="primary" onClick={increment} />
         ) : (
-          <Button text="Submit" variant="primary" onClick={onSubmit} {...submitButton} />
+          <Button
+            text="Submit"
+            variant="primary"
+            onClick={onSubmit}
+            {...submitButton}
+          />
         )}
       </div>
     </div>
