2188610136569331d9886afd8bcec9f33d21ee4a
Brendan McGuire <brendan@bren.app>
2021-11-15 15:28:09 -0500

    Use backend API for reccomendation

--- pages/suggest.tsx
+++ pages/suggest.tsx
@@ -8,7 +8,7 @@ import React, { useEffect, useState } from "react";
 import { Header } from "@components/Header";
 import Head from "next/head";
 import { motion, useCycle } from "framer-motion";
-import { Select } from "@components/Input";
+import { Button, Select } from "@components/Input";
 import { FivePointScale } from "@lib/server/database";
 import Image from "next/image";
 import { BUILDINGS } from "@lib/client/data";
@@ -18,22 +18,54 @@ import clothing2 from "@static/clothing/clothing2.png";
 const images = [clothing1, clothing2];
 
 import { favorites } from "@lib/client/favorites";
-
-const itemsA = [0, 1, 2, 3];
-const itemsB = [2, 0, 3, 1];
-const itemsC = [3, 2, 1, 0];
-const itemsD = [1, 3, 0, 2];
+import Spinner from "@components/Spinner";
+import PopupID from "@components/popup/id";
 
 export default function Suggest() {
   const [clothing, setClothing] = useState(-1);
-  const [items, setItems] = useCycle(itemsA, itemsB, itemsC, itemsD);
+  const [spinner, setSpinner] = useState(false);
+  const [items, setItems] = useState<number[]>([]);
+  const [fitness, setFitness] = useState<{ [id: number]: number }>({});
 
   useEffect(() => {
-    setItems();
-  }, [clothing]);
+    if (clothing === -1) {
+      return;
+    }
+    if (!spinner) {
+      return;
+    }
+
+    async function load(): Promise<number[]> {
+      const rooms = favorites.map((f) => `&rooms=${f.PointSliceID}`);
+      const api = `/api/suggest?id=${localStorage.getItem(
+        "id"
+      )}&clothing_level=${clothing}${rooms.join("")}`;
+
+      const result: { [id: number]: number } = await fetch(api).then((res) =>
+        res.json()
+      );
+      const order = favorites
+        .map((room, i) => i)
+        .sort(
+          (a, b) =>
+            result[favorites[b].PointSliceID] -
+            result[favorites[a].PointSliceID]
+        );
+
+      setFitness(result);
+
+      return order;
+    }
+
+    load().then((result: number[]) => {
+      setItems(result);
+      setSpinner(false);
+    });
+  }, [spinner]);
 
   return (
     <main className="min-h-screen bg-gray-100">
+      <PopupID />
       <Head>
         <title>Suggest A Room</title>
       </Head>
@@ -64,50 +96,67 @@ export default function Suggest() {
             </div>
           )}
         />
-
-        { clothing !== -1 && (<div>
-          <h1 className="mt-8 mb-4 uppercase text-lg text-center text-gray-700">Your Suggested Rooms</h1>
-          {items.map((item, index) => (
-            <motion.div
-              key={item}
-              layout
-              initial="false"
-              transition={{ type: "spring", stiffness: 350, damping: 25 }}
-              className="rounded-3xl bg-white shadow-md px-6 py-6 mb-6 flex items-center hover:shadow-xl transition-shadow cursor-pointer"
-            >
-               <div className="rounded-lg mr-6 overflow-hidden flex items-center">
-                <h1 className="text-xl text-orange">#{index + 1}</h1>
-              </div>
-              <div className="h-12 w-12 bg-gray-500 rounded-lg mr-6 overflow-hidden">
-                <img
-                  src={`/buildings/${favorites[item].BLG}.png`}
-                  alt={BUILDINGS[favorites[item].BLG]}
-                  className="h-full rounded-lg max-w-max"
-                />
-              </div>
-              <div className="flex-1">
-                <h3 className="text-lg">{favorites[item].Alias}</h3>
-                <h5 className="text-base text-gray-500">
-                  {BUILDINGS[favorites[item].BLG]}
-                </h5>
-              </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-4 text-orange"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+        <Button
+          text="Recommend"
+          variant={clothing === -1 ? "disabled" : "primary"}
+          className="w-42 ml-auto mr-6 bg-orange text-white"
+          onClick={() => clothing != -1 && setSpinner(true)}
+          before={() => (spinner ? <Spinner /> : null)}
+        />
+        {clothing !== -1 && items.length > 0 && (
+          <div>
+            <h1 className="mt-8 mb-4 uppercase text-lg text-center text-gray-700">
+              Your Suggested Rooms
+            </h1>
+            {items.map((item, index) => (
+              <motion.div
+                key={item}
+                layout
+                initial="false"
+                transition={{ type: "spring", stiffness: 350, damping: 25 }}
+                className="rounded-3xl bg-white shadow-md px-6 py-6 mb-6 flex items-center hover:shadow-xl transition-shadow cursor-pointer"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
-            </motion.div>
-          ))}
-        </div>)}
+                <div className="rounded-lg mr-6 overflow-hidden flex items-center">
+                  <h1 className="text-xl text-orange">#{index + 1}</h1>
+                </div>
+                <div className="h-12 w-12 bg-gray-500 rounded-lg mr-6 overflow-hidden">
+                  <img
+                    src={`/buildings/${favorites[item].BLG}.png`}
+                    alt={BUILDINGS[favorites[item].BLG]}
+                    className="h-full rounded-lg max-w-max"
+                  />
+                </div>
+                <div className="flex-1">
+                  <h3 className="text-lg flex items-center">
+                    {favorites[item].Alias}
+                    <span className="text-orange ml-2 text-sm">
+                      (
+                      {(fitness[favorites[item].PointSliceID] * 100).toFixed(2)}
+                      % Match)
+                    </span>
+                  </h3>
+                  <h5 className="text-base text-gray-500">
+                    {BUILDINGS[favorites[item].BLG]}
+                  </h5>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 mr-4 text-orange"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+              </motion.div>
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
